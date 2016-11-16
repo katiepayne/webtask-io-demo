21b3b88a3a0424e4be0dc2af6a5f3ee86d155fea
Katie Sutherland <katie.sutherland93@icloud.com>
2016-11-16 09:41:35 -0500

    Added comments / cleaned up code.

--- webtask.js
+++ webtask.js
@@ -1,7 +1,15 @@
-
+// Contents of module.exports are returned during require.
 module.exports = function(context, callback) {
+
+	// load request module
 	var request = require('request');
 
+    /**
+     * captures errors and stores them in db using mLab.
+     * @param database - Name of database
+     * @param collection - Name of collection
+     * @param apiKey - apiKey generated from mLab.
+     */
 	function mLabConnection( database, collection, apiKey ) {
 
 		/**
@@ -23,7 +31,7 @@ module.exports = function(context, callback) {
 	    	function(error, response, body) {
 				// Check for errors.
 				if ( error )
-					return { error: error, response: response: body: body };
+					return { error: error, response: response, body: body };
 
 				// No Errors.
 				callback(null, JSON.stringify(response));
@@ -32,10 +40,15 @@ module.exports = function(context, callback) {
 		}
 	}
 
-	function twilio(userKey, password) {
+    /**
+     * Sends message via Twilio
+     * @param userKey - Twilio userKey
+     * @param password - Twilio password
+     */
+    function twilio(userKey, password) {
 
 	    /**
-	    * Sends a message via twilio.
+	    * Function that sends message.
 	    * @param fromPhone - Phone number of recipient
 	    * @param toPhone - Phone number of recipient
 	    * @param message - Message to send.
@@ -62,17 +75,26 @@ module.exports = function(context, callback) {
 	    }
 	}
 
+	// Instantiate the Twilio helper object.
 	var twilio = new twilio(context.data.TWILIO_USER, context.data.TWILIO_PASS);
-	
+
+    // Instantiate the mLab database connection.
 	var mLab = new mLabConnection(context.data.MLAB_DBNAME, context.data.MLAB_COL, context.data.MLAB_APIKEY );
 
 	// Send a message.
-	var result = twilio.sendMessage(context.data.FROM_PHONE, context.data.TO_PHONE, new Date().toLocaleDateString + ": This is a test.") 
+	var result = twilio.sendMessage(context.data.FROM_PHONE, context.data.TO_PHONE, new Date().toLocaleDateString() + ": This is a test.");
 	
 	// A successful result will be false, if truthy -- there are errors.
 	if( result )
 
+	    // Make a task object.
+        var task = {};
+
+	    // Use the error key and the current time as the key for the error entry.
+        // Stringify the result object to be used as debug info.
+        task[ context.data.ERROR_KEY + "-" + new Date().toLocaleDateString() ] = JSON.stringify(result);
+
 		// Errors are present, log them to the mLab db.
-		mLab.saveTask( {context.data.ERROR_KEY: JSON.stringify(result) } );
+		mLab.saveTask( task );
 	}
 }
